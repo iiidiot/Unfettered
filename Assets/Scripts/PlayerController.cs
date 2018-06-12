205fa9bf8ed665a7d9339bdb03ea954914f35122
nothing0n <nothing0n@foxmail.com>
2018-06-12 20:04:23 +0800

    setup idle & idleJump amimation for M1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        InputControl();
+	}
+
+    void InputControl()
+    {
+        AnimatorStateInfo animatorInfo;
+        animatorInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && animatorInfo.IsName("M1_Idle"))
+        {
+            animator.SetBool("isIdle", false);
+            animator.SetBool("isIdleJump", true);
+        }
+
+        if ((animatorInfo.normalizedTime > 1.0f) && (animatorInfo.IsName("M1_IdleJump")))
+        {
+            animator.SetBool("isIdleJump", false);
+            animator.SetBool("isIdle", true);
+        }
+    }
+}
